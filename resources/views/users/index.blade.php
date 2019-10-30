8e75f601ecad541c24348d17c72865d55c2b6bf0
M4nthas <mantas.daunoravicius@ktu.edu>
2019-10-30 12:34:34 +0200

    u

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+     <div id="app">
+        <div class="container mt-3">
+          <h1>Users</h1>
+          <users>
+          </users>
+        </div>
+      </div>
+@endsection
