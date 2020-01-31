1049213e703874e6dca1a536f97a9c4088afc3ce
M4nthas <mantas.daunoravicius@ktu.edu>
2020-01-31 20:04:11 +0200

    https fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        \URL::forceScheme('https');
+        if($this->app->environment() === 'production')
+        { 
+            $this->app['request']->server->set('HTTPS', true); 
+        }
     }
 }
