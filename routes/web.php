ccbcf377b51fdea901a1b7736122f491dae5722d
M4nthas <mantas.daunoravicius@ktu.edu>
2020-01-31 20:06:54 +0200

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
 
 Route::get('/home', function () {
     return view('home');
-})->middleware('auth');
+});
 
 Route::get('/about', function () {
     return view('pages.about');
@@ -43,20 +43,20 @@ Route::get('/ads/{ad}', function () {
 
 Route::get('/orders', function () {
     return view('orders.index');
-})->middleware('auth');
+});
 
 Route::get('/myorders', function () {
     return view('orders.userOrders');
-})->middleware('auth');
+});
 
 Route::get('/orders/{order}', function () {
     return view('orders.show');
-})->middleware('auth');
+});
 
 Route::get('/users', function () {
     return view('users.index');
-})->middleware('auth');
+});
 
 Route::get('/users/{user}', function () {
     return view('users.show');
-})->middleware('auth');
+});
