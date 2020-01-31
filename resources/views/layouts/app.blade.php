a2dbb8742f0f41bc4f466ccb4fb4470252f0817c
M4nthas <mantas.daunoravicius@ktu.edu>
2020-01-31 17:06:39 +0200

    styles fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="/css/app.css" rel="stylesheet">
     <style>
         img {
             width: 100%;
@@ -30,10 +30,8 @@
 </head>
 <body>
     <div id="app">
-        <main>
-            
-            <navbar></navbar>
-                
+        <main>            
+            <navbar></navbar>                
             <div class="container mt-5">
                 @include('inc.messages')
 
@@ -49,6 +47,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="js/app.js"></script>
 </body>
 </html>
